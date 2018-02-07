ef008d2614722505bbad9025f06a8ece12729339
Jarod Lam <jarod@Jarods-MacBook-Pro.local>
2018-02-07 15:47:47 +1000

    Re-create the socket every loop

--- computer/photoresistor-server/photoresistor-server.py
+++ computer/photoresistor-server/photoresistor-server.py
@@ -22,14 +22,9 @@ print("Client IPv6 address:", UDP_CLIENT_IP)
 print("Client port:", UDP_CLIENT_PORT)
 print("Message:", MESSAGE)
 
-# Setup the UDP socket
-(family, socktype, proto, canonname, sockaddr) = socket.getaddrinfo(UDP_CLIENT_IP, 3000, socket.AF_INET6, socket.SOCK_DGRAM)[0]
-print(socket.getaddrinfo(UDP_CLIENT_IP, 3000, socket.AF_INET6, socket.SOCK_DGRAM)[0])
-sock = socket.socket(family, socktype, proto)
-
 # Function for sending a message to the target IP
 def send_message():
-    threading.Timer(MESSAGE_INTERVAL, send_message).start()
+    #threading.Timer(MESSAGE_INTERVAL, send_message).start()
     try:
         sock.sendto(MESSAGE.encode(), sockaddr)
         print("Sent message:", MESSAGE)
@@ -37,14 +32,19 @@ def send_message():
         print(format(err))
         pass
 
-# Start sending messages every x seconds
-send_message()
-
-# Listen for any messages and print them in the terminal
+# Setup the UDP socket
 while True:
+    (family, socktype, proto, canonname, sockaddr) = socket.getaddrinfo(UDP_CLIENT_IP, 3000, socket.AF_INET6, socket.SOCK_DGRAM)[0]
+    #print(socket.getaddrinfo(UDP_CLIENT_IP, 3000, socket.AF_INET6, socket.SOCK_DGRAM)[0])
+    sock = socket.socket(family, socktype, proto)
+
+    # Start sending messages every x seconds
+    send_message()
+
+    # Listen for any messages and print them in the terminal
     data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
     value = int.from_bytes(data, byteorder=sys.byteorder)
     print("Received message:", value)
-    
+
     percent = -(value / 32764 * 100) + 100
     ubidots_variable.save_value({'value': percent})
